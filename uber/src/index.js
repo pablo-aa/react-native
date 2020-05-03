f0a01f5358439c3844c5a3bdedce8d602fad095f
Pablo A <pablo_aa@MacBook-Air-de-Pablo.local>
2020-05-03 09:54:14 -0300

    em produção

--- uber/src/index.js
+++ uber/src/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import MapView from 'react-native-maps'
-
-import { View } from 'react-native';
-
+import Map from './components/Map';
 // import { Container } from './styles';
 
 const App = () => (
-<View style = {{flex: 1}}>
-    <MapView 
-        style={{flex:1}}
-        region={{
-           latitude: -15.8664847,
-           longitude: -47.9715547,
-           latitudeDelta: 0.0143,
-           longitudeDelta: 0.0134,
-        }}
-        showsUserLocation
-        loadingEnabled
-    />
-</View>
+    <Map />
 )
 
 
